4854322e838f944673758ae090b16b0ca03d41a5
BazzalSeed <cdzengpeiyun@gmail.com>
2016-04-24 11:53:04 -0500

    Step3 Done

--- MileStone2/step3.Visulization.py
+++ MileStone2/step3.Visulization.py
@@ -2,24 +2,27 @@
 import plotly.plotly as py
 import pandas as pd
 
-df = pd.read_csv('sample_geo.csv')
-df.head()
+df_all = pd.read_csv('step3.filteredAll.csv')
 
-df['text'] = "ID: " + df['LocationID'].astype(str)
+df_usa = pd.read_csv('step3.filteredUnitedStates.csv')
 
+
+df_all['text'] = "ID: " + df_all['Id'].astype(str)
+df_usa['text'] = "ID: " + df_usa['Id'].astype(str)
 
 scl = [[0, "rgb(5, 10, 172)"], [0.35, "rgb(40, 60, 190)"], [0.5, "rgb(70, 100, 245)"],
        [0.6, "rgb(90, 120, 245)"], [0.7, "rgb(106, 137, 247)"], [1, "rgb(220, 220, 220)"]]
 
-data = [dict(
+trace1 = [dict(
     type='scattergeo',
-    lat=df['Latitude'],
-    lon=df['Longitude'],
-    text=df['text'],
+    lat=df_all['Lat'],
+    lon=df_all['Lon'],
+    text=df_all['text'],
     mode='markers',
     marker=dict(
         size=3,
         opacity=0.6,
+        symbol = 'square',
         reversescale=True,
         autocolorscale=False,
         line=dict(
@@ -28,22 +31,41 @@ data = [dict(
         )
 
     ))]
+trace2 = [dict(
+    type='scattergeo',
+    lat=df_usa['Lat'],
+    lon=df_usa['Lon'],
+    text=df_usa['text'],
+    mode='markers',
+    marker=dict(
+        size=3,
+        opacity=0.6,
+        reversescale=True,
+        autocolorscale=False,
+        line=dict(
+            width=0.5,
+            color='rgba(0, 0, 0)'
+        )
 
+    ))]
+data = [trace1,trace2]
 layout = dict(
-        scope='usa',
-        title = 'Sample Geo Data Visulization',
 
-        geo = dict(
-            scope='usa',
-            projection=dict( type='albers usa' ),
-            showland = True,
-            landcolor = "rgb(250, 250, 250)",
-            subunitcolor = "rgb(217, 217, 217)",
-            countrycolor = "rgb(217, 217, 217)",
-            countrywidth = 0.5,
-            subunitwidth = 0.5
-        ),
-    )
+    title='Step3.Visulization',
 
-fig = dict(data=data, layout=layout)
-url = py.plot(fig, validate=False, filename='d3-airports')
+    geo=dict(
+        scope = 'usa',
+        projection=dict(type='albers usa'),
+        showland=True,
+        landcolor="rgb(250, 250, 250)",
+        subunitcolor="rgb(217, 217, 217)",
+        countrycolor="rgb(217, 217, 217)",
+        countrywidth=0.5,
+        subunitwidth=0.5
+    ),
+)
+
+fig = dict(data=trace1, layout=layout)
+fig_ = dict(data=trace2, layout=layout)
+url = py.plot(fig, validate=False, filename='all')
+url = py.plot(fig_, validate=False, filename='usa')
