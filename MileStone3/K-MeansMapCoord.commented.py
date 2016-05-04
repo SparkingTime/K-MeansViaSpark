0346e699c2c981c1f07e5deb519793ed548ff292
mengnalin <mengnalin@wustl.edu>
2016-05-04 00:45:57 -0500

    Update K-MeansMapCoord.commented.py

--- MileStone3/K-MeansMapCoord.commented.py
+++ MileStone3/K-MeansMapCoord.commented.py
@@ -41,25 +41,28 @@ def closestCenterEuclidean(p, centers):
             bestIndex = i
     return bestIndex
 
-
+#retrieve map output in greate circle distance(key: index of the piont, value: (point, 1, point in list format))
 def mapHaversine(p):
     return (closestCenterHaversine(p, kPoints), (p, 1, p.tolist()))
 
-
+#retrieve map output in Euclidean distance(key: index of the piont, value: (point, 1, point in list format))
 def mapEuclidean(p):
     return (closestCenterEuclidean(p, kPoints), (p, 1, p.tolist()))
 
+#reduce method 
 def testMethod(p1v, p2v):
     return (p1v[0] + p2v[0], p1v[1] + p2v[1],p1v[-1] + p2v[-1])
 
 
 if __name__ == "__main__":
 
+    # input checking
     if len(sys.argv) != 6:
         print("Usage: kmeans <file> <k> <convergeDist> <DistanceMethod> <dir> ",
               file=sys.stderr)
         exit(-1)
 
+    #input retrieve
     sc = SparkContext("local[5]",appName="PythonKMeans")
     lines = sc.textFile(sys.argv[1])
     data = lines.map(generateVector).cache()
@@ -67,13 +70,16 @@ if __name__ == "__main__":
     convergeDist = float(sys.argv[3])
     DistanceMethod = sys.argv[4]
     dirName = sys.argv[5]
-
+    
+    #choose k initial cluster centers at random
     kPoints = data.takeSample(False, K, 1)
     it = 0
     tempDist = float('inf')
     global_min = float('inf')
     while tempDist > convergeDist and it < 1:
+        #states checking 
         print("<-@@@@@@@@@@@@@@@@@@@@@@-----------------------------------------------------" + "Distance: "+ str(tempDist) + " on iteration "+ str(it) +"------------------------------------------------------@@@@@@@@@@@@@@@@@@@@@@->")
+        #mapper output
         if(DistanceMethod == "Euclidean"):
             closest = data.map(mapEuclidean)
         elif(DistanceMethod == "GreateCircle"):
@@ -81,8 +87,10 @@ if __name__ == "__main__":
         else:
             print("WTF DID YOU SAY? MAN, GreateCircle/Euclidean please")
             exit(-1)
+        #reducer output
         pointStats = closest.reduceByKey(testMethod,5) 
         #pointStats = closest.reduceByKey(testMethod) 
+        #new cluster centers
         newPoints = pointStats.map(
             lambda st: (st[0], st[1][0] / st[1][1])).collect()
         if(DistanceMethod == "Euclidean"):
@@ -92,6 +100,7 @@ if __name__ == "__main__":
             tempDist = sum(hv(kPoints[iK], p) for (iK, p) in newPoints)
         for (iK, p) in newPoints:
             kPoints[iK] = p
+        #update temperary distance 
         if tempDist < global_min:
     	    pointsInfo = pointStats
             global_min = tempDist
