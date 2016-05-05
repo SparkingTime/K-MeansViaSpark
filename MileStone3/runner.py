c9b32cf549eafef93e842a758e4890de435025aa
cogle <thecogle@gmail.com>
2016-05-04 23:43:19 -0700

    Convergence verified

--- MileStone3/runner.py
+++ MileStone3/runner.py
@@ -4,19 +4,19 @@
 import os
 
 #Runs the Device Status Data
-os.system("spark-submit K-MeansMapCoord.deviceStatus.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/filteredStatus.csv 5 .1 GreateCircle @1.DeviceLocationDataClusters_GreateCircle")
-os.system("spark-submit K-MeansMapCoord.deviceStatus.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/filteredStatus.csv 5 .1 Euclidean @1.DeviceLocationDataClusters_Euclidian")
+#os.system("spark-submit K-MeansMapCoord.deviceStatus.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/filteredStatus.csv 5 .1 GreateCircle @1.DeviceLocationDataClusters_GreateCircle")
+#os.system("spark-submit K-MeansMapCoord.deviceStatus.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/filteredStatus.csv 5 .1 Euclidean @1.DeviceLocationDataClusters_Euclidian")
 
 
 #Runs the All Data k = 2,3,4
-#os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 2 35 Euclidean @1.GlobalClusters_Euclidian_k2")
 #os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 2 35 GreateCircle @1.GlobalClusters_GreateCircle_k2")
+os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 3 .1 Euclidean @3.GlobalClusters_Euclidian_k3")
+os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 2 .1 Euclidean @3.GlobalClusters_Euclidian_k2")
 
 #os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 3 35 Euclidean @1.GlobalClusters_Euclidian_k3")
 #os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 3 35 GreateCircle @1.GlobalClusters_GreateCircle_k3")
 
-os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 4 .1 Euclidean @1.GlobalClusters_Euclidian_k4")
-os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 4 .1 GreateCircle @1.GlobalClusters_GreateCircle_k4")
+#os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 4 .1 GreateCircle @1.GlobalClusters_GreateCircle_k4")
 
 #os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 5 .1 Euclidean @1.GlobalClusters_Euclidian_k6")
 #os.system("spark-submit K-MeansMapCoord.DBpedia.py file:///home/training/K-MeansViaSpark/MileStone3/step3.Input/step3.filteredAll.csv 5 .1 GreateCircle @1.GlobalClusters_GreateCircle_k6")
